70be6ce53169d845524c60aa2e1dd4d4fb790ab9
MBAEduHans <hans@MBAEduHans.local>
2026-05-13 15:45:48 +0900

    UserモデルとEnsureUserIsActiveミドルウェアを追加
    - UserモデルにHasApiTokens traitを追加してAPIトークン発行を可能化
    - EnsureUserIsActiveミドルウェアで凍結アカウント(status=Inactive)を拒否
    - bootstrap/app.phpにactive.userエイリアスを登録

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,13 +10,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 #[Fillable(['email', 'name', 'password', 'status'])]
 #[Hidden(['password'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * Get the attributes that should be cast.
